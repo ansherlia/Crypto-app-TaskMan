ae3aea0d6cf2980c12b1ac9885a85404687aefe8
reza hagju <ansherlireza2020@gmail.com>
2024-07-03 16:35:27 +0330

    paginat component was created and styled

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import { apiAndUrl } from "../../services/apiandReq";
 import TableCoins from "../modules/TableCoins";
+import Pagination from "../modules/Pagination";
 function HomePage() {
+	const [page, setPage] = useState(1);
 	const [coins, setCoins] = useState([]);
 	useEffect(() => {
 		const coinsFetchData = async () => {
-			const respone = await fetch(apiAndUrl());
+			const respone = await fetch(apiAndUrl(page));
 			const json = await respone.json();
 			console.log(json);
 			setCoins(json);
 		};
 		coinsFetchData();
 		console.log(coins);
-	}, []);
+	}, [page]);
 	return (
 		<div>
 			<TableCoins coins={coins} />
+			<Pagination page={page} setPage={setPage} />
 		</div>
 	);
 }
